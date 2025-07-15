52421a980d26c609c071d1f6d0d7cf476f7be4d0
SeongChoel <83538651+SeongChoel@users.noreply.github.com>
2025-07-15 12:37:14 +0900

    Feat#1 (#46)
    * feat : BaseEntity, BaseTime 추가
    
    * fix : gitignore 파일삭제
    
    * fix2: gitignore 추가
    
    * fix3: out 삭제
    
    * fix : basetime이름 수정
    
    ---------
    
    Co-authored-by: SeongChoel <tldtlddl1234@gmail.com>

---
+++ src/main/java/com/example/demo/global/entity/BaseEntityWithTime.java
@@ -0,0 +1,27 @@
+package com.example.demo.global.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@SuperBuilder
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntityWithTime extends BaseEntity {
+    @CreatedDate
+    @Setter(AccessLevel.PRIVATE)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Setter(AccessLevel.PRIVATE)
+    private LocalDateTime modifiedDate;
+}
