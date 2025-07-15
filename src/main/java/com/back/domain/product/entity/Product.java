85031518ab7c78f549df97b1a1a524e1bd57678e
SeongChoel <tldtlddl1234@gmail.com>
2025-07-15 15:17:12 +0900

    feat: category lenght 변경

--- src/main/java/com/back/domain/product/entity/Product.java
+++ src/main/java/com/back/domain/product/entity/Product.java
@@ -20,7 +20,7 @@ public class Product extends BaseEntityWithTime {
     private int price;
     @Column(length = 100, unique = true)
     private String imageUrl;
-    @Column(length = 10)
+    @Column(length = 30)
     private String category;
     @Column(length = 100)
     private String description;
