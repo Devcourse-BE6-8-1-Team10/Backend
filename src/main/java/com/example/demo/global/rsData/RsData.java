898daaacbecb2028243cab22e5b9f400444c1de6
yongho9064 <dydgmldydgh@naver.com>
2025-07-15 12:13:46 +0900

    feat: RsData 구현

---
+++ src/main/java/com/example/demo/global/rsData/RsData.java
@@ -0,0 +1,19 @@
+package com.example.demo.global.rsData;
+
+public record RsData<T>(Integer code, String message, T data) {
+
+    public static <T> RsData<T> of(Integer code, String message, T data) {
+        return new RsData<>(code, message, data);
+    }
+
+    // 성공 편의 메소드
+    public static <T> RsData<T> successOf(T data) {
+        return of(200, "success", data);
+    }
+
+    // 실패 편의 메소드
+    public static <T> RsData<T> failOf(T data) {
+        return of(500, "fail", data);
+    }
+
+}
