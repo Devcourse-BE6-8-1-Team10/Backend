9ebc7a8f559d639da74c7bedd4a24a83a8b7d003
SeongChoel <83538651+SeongChoel@users.noreply.github.com>
2025-07-15 15:29:46 +0900

    [feat] : 상품entity작성#4 (#52)
    * feat : 상품entity작성
    
    * feat: category lenght 변경
    
    ---------
    
    Co-authored-by: SeongChoel <tldtlddl1234@gmail.com>

---
+++ src/main/java/com/back/domain/product/entity/Product.java
@@ -0,0 +1,29 @@
+package com.back.domain.product.entity;
+
+import com.back.global.entity.BaseEntityWithTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class Product extends BaseEntityWithTime {
+
+    @Column(length = 100, unique = true)
+    private String productName;
+    private int price;
+    @Column(length = 100, unique = true)
+    private String imageUrl;
+    @Column(length = 30)
+    private String category;
+    @Column(length = 100)
+    private String description;
+    private boolean orderable;
+
+}
