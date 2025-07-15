98956ed8261da45b13bac703abaea92067c750cf
yongho9064 <dydgmldydgh@naver.com>
2025-07-15 12:38:49 +0900

    feat: RsData 구현

--- src/main/java/com/example/demo/order/controller/DummyController.java
+++ src/main/java/com/example/demo/order/controller/DummyController.java
@@ -1,4 +1,19 @@
 package com.example.demo.order.controller;
 
+import com.example.demo.global.rsData.RsData;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DummyController {
+
+    @GetMapping("/dummy")
+    public RsData<String> dummy() {
+        return RsData.of("200", "success", null);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "반가워요";
+    }
 }
