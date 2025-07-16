0cea0d978016fc7ab905a3c10afbc59dd1e84170
Hyeok Jin Kim <71928299+EpicFn@users.noreply.github.com>
2025-07-16 16:50:25 +0900

    [feat] 멤버 추가 (회원가입) 구현 #1 (#66)
    * example 폴더 삭제
    
    * 실패하는 테스트 코드 작성
    
    * 회원가입 endpoint 구현
    
    * feat : 테스트 케이스 통과
    
    * refactor : RsData의 code 타입을 int로 변경
    
    * fix : code-rabbit review 반영
    
    ---------
    
    Co-authored-by: EpicFn <rlagurwls056@gmail.com>

---
+++ src/test/java/com/back/domain/member/member/controller/MemberControllerTest.java
@@ -0,0 +1,60 @@
+package com.back.domain.member.member.controller;
+
+import com.back.domain.member.member.entity.Member;
+import com.back.domain.member.member.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class MemberControllerTest {
+    @Autowired
+    private MemberService memberService;
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("회원 가입")
+    void join() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/members")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "email": "testuser@gmail.com",
+                                            "password": "testpassword",
+                                            "name": "테스트 유저"
+                                        }
+                                        """.stripIndent())
+                )
+                .andDo(print());
+
+        Member member = memberService.findByEmail("testuser@gmail.com").get();
+
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("join"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.code").value(201))
+                .andExpect(jsonPath("$.message").value("%s님 환영합니다. 회원가입이 완료되었습니다.".formatted(member.getName())))
+                .andExpect(jsonPath("$.data.id").value(member.getId()))
+                .andExpect(jsonPath("$.data.email").value(member.getEmail()))
+                .andExpect(jsonPath("$.data.name").value(member.getName()));
+    }
+
+}
