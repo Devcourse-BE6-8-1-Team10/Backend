fec38ff6fac7cc755d069acd5f0abcfcfe86cfe0
EpicFn <rlagurwls056@gmail.com>
2025-07-16 14:16:16 +0900

    feat : Address Entity 보강

--- src/main/java/com/back/domain/member/address/entity/Address.java
+++ src/main/java/com/back/domain/member/address/entity/Address.java
@@ -10,13 +10,31 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class Address extends BaseEntity {
+    // ------------ [필드] ------------
     @Column(nullable=false)
     private String address;
 
-    private Boolean isDefault = false;
+    @Column(nullable=false)
+    private Boolean isDefault;
 
     @ManyToOne(fetch = FetchType.LAZY) //지연 로딩으로 성능 최적화
     @JoinColumn(name= "member_id", referencedColumnName= "id") //외래키 설정
     private Member member;
+
+    // ------------ [생성자] ------------
+    public Address(String address, Boolean isDefault, Member member) {
+        this.address = address;
+        this.isDefault = isDefault;
+        this.member = member;
+    }
+
+    public Address(String address, Member member){
+        this(address, false, member);
+    }
+
+    // ------------ [메서드] ------------
+    public void setDefault(Boolean isDefault) {
+        this.isDefault = isDefault;
+    }
 
 }
