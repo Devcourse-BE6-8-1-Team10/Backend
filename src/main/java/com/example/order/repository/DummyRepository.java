287cdce6226599e36b4065f5831c0e7c135d7b68
EpicFn <rlagurwls056@gmail.com>
2025-07-16 14:50:11 +0900

    example 폴더 삭제

--- src/main/java/com/example/order/repository/DummyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.order.repository;
-
-import com.example.order.entity.DummyEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DummyRepository extends JpaRepository<DummyEntity, Long> {
-}
