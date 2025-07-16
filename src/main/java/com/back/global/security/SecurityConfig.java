800a2ea10423e5cc8b275f80caf72efef8bcda6a
EpicFn <rlagurwls056@gmail.com>
2025-07-16 20:09:37 +0900

    feat : Spring security 도입

---
+++ src/main/java/com/back/global/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.back.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/favicon.ico").permitAll()
+                .requestMatchers("/h2-console/**").permitAll()
+                .anyRequest().permitAll()
+            );
+
+        return http.build();
+
+    }
+
+}
