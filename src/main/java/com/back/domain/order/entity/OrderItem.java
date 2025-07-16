6bb1fccb26eead8a2e007a82999a1cd8913fe982
rladbstn0 <140579355+rladbstn0@users.noreply.github.com>
2025-07-16 10:37:57 +0900

    [fect] 주문 DTO 생성#1 (#56)
    * [fect] 주문 DTO 생성
    
    * [refactor] Schema ì작성

--- src/main/java/com/back/domain/order/entity/OrderItem.java
+++ src/main/java/com/back/domain/order/entity/OrderItem.java
@@ -1,14 +1,14 @@
 package com.back.domain.order.entity;
 
+import com.back.domain.product.entity.Product;
 import com.back.global.entity.BaseEntity;
-import com.back.global.entity.BaseEntityWithTime;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-
-import java.awt.*;
 
 
 @Entity
@@ -21,16 +21,16 @@ public class OrderItem extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     private Order order;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    private Menu menu;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Product product;
 
     private int count;
 
     private int price;
 
-    public OrderItem(Order order, Menu menu, int count, int price) {
+    public OrderItem(Order order, Product product, int count, int price) {
         this.order = order;
-//        this.menu = menu;
+        this.product = product;
         this.count = count;
         this.price = price;
     }
