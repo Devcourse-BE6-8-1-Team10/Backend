4c60ce32e2556efc663ba128f8fe9b90e9ef47e2
user.email <younsoo6103@naver.com>
2025-07-16 09:31:57 +0900

    [refactor] Schema ì작성

--- src/main/java/com/back/domain/order/dto/OrderDto.java
+++ src/main/java/com/back/domain/order/dto/OrderDto.java
@@ -1,24 +1,31 @@
 package com.back.domain.order.dto;
 
 import com.back.domain.order.entity.Order;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.lang.NonNull;
 
 import java.time.LocalDateTime;
 
+@Schema(description = "주문 정보 DTO")
 public record OrderDto(
+        @Schema(description = "주문 ID")
         @NonNull Long id,
+        @Schema(description = "주문자 이메일")
         @NonNull String customerEmail,
-        @NonNull String customerAddress,
+        @Schema(description = "주문 날짜")
+        @NonNull LocalDateTime createdDate,
+        @Schema(description = "처리 상태")
         @NonNull String state,
-        @NonNull LocalDateTime createdDate
-) {
+        @Schema(description = "주문 주소")
+        @NonNull String customerAddress
+        ) {
     public OrderDto(Order order) {
         this(
                 order.getId(),
                 order.getCustomerEmail(),
-                order.getCustomerAddress(),
+                order.getCreatedDate(),
                 order.getState(),
-                order.getCreatedDate()
+                order.getCustomerAddress()
         );
     }
 }
--- src/main/java/com/back/domain/order/dto/OrderItemDto.java
+++ src/main/java/com/back/domain/order/dto/OrderItemDto.java
@@ -1,13 +1,20 @@
 package com.back.domain.order.dto;
 
 import com.back.domain.order.entity.OrderItem;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.lang.NonNull;
 
+@Schema(description = "상세 주문 정보 DTO")
 public record OrderItemDto(
+        @Schema(description = "주문 아이템 ID")
         @NonNull Long id,
+        @Schema(description = "주문 ID")
         @NonNull Long orderId,
+        @Schema(description = "상품 ID")
         @NonNull Long productId,
+        @Schema(description = "수량")
         @NonNull int count,
+        @Schema(description = "낱개 가격")
         @NonNull int price
 ) {
     public OrderItemDto(OrderItem orderItem) {
