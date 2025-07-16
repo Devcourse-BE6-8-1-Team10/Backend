dadb4ac10a81df11dd5d982340ad19e502f71f3d
SeongChoel <83538651+SeongChoel@users.noreply.github.com>
2025-07-16 12:38:56 +0900

    [Feat] 메뉴(상품) 전체 조회 #1 (#57)
    * chore: validation,lombok추가
    
    * core:validation,lombok추가
    
    * RsData없이 상품다건조회작성
    
    * feat : productDto 작성
    
    * feat: PageDTO , 상품 전체목록 페이징, 상품 생성
    
    * feat : Pagedto refactoring
    
    * feat : 상품 전제조회 유효성 검사, 실패처리 수정
    
    * feat : ProductController 테스트 케이스 작성
    
    * feat : ProductController 테스트케이스 잘못된 체이지
    
    * feat : ProductController 테스트케이스 수정
    
    ---------
    
    Co-authored-by: SeongChoel <tldtlddl1234@gmail.com>

---
+++ src/main/java/com/back/domain/product/service/ProductService.java
@@ -0,0 +1,50 @@
+package com.back.domain.product.service;
+
+import com.back.domain.product.entity.Product;
+import com.back.domain.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public Product create(String productName, int price, String imageUrl,
+                          String category, String description, boolean orderable) {
+
+        if(productName==null || productName.trim().isEmpty()){
+            throw new IllegalArgumentException("상품명은 필수입니다.");
+        }
+        if(price<0) {
+            throw new IllegalArgumentException("가격은 0 이상이어야 합니다.");
+        }
+
+        return productRepository.save(
+                Product
+                        .builder()
+                        .productName(productName)
+                        .price(price)
+                        .imageUrl(imageUrl)
+                        .category(category)
+                        .description(description)
+                        .orderable(orderable)
+                        .build()
+        );
+
+    }
+
+    //컨트롤러 에서 유효성 검증함 , 여기서 생략
+    public Page<Product> getItems(int page, int pageSize) {
+        PageRequest pageRequest = PageRequest.of(page-1, pageSize);
+
+        return productRepository.findAll(pageRequest);
+    }
+
+    public long count() {
+        return productRepository.count();
+    }
+}
