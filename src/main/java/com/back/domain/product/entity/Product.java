53bf80db789385fb5cda1b2a2da961c32b48034c
SeongChoel <83538651+SeongChoel@users.noreply.github.com>
2025-07-17 09:44:06 +0900

    feat: 상품 수정기능, 테스트 케이스 2개 작성 (#71)
    Co-authored-by: SeongChoel <tldtlddl1234@gmail.com>

--- src/main/java/com/back/domain/product/entity/Product.java
+++ src/main/java/com/back/domain/product/entity/Product.java
@@ -6,10 +6,12 @@ import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
