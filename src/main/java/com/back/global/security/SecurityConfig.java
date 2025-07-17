8b8b6195bc320c0cd67df6a78f3a0ac6c9484c46
EpicFn <rlagurwls056@gmail.com>
2025-07-17 09:28:11 +0900

    security config 설정

--- src/main/java/com/back/global/security/SecurityConfig.java
+++ src/main/java/com/back/global/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.back.global.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,8 +15,12 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/favicon.ico").permitAll()
-                .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers("/favicon.ico").permitAll() // 파비콘 접근 허용 (검색 엔진 최적화)
+                .requestMatchers("/h2-console/**").permitAll() // H2 콘솔 접근 허용
+                .requestMatchers("/api/members/login", "/api/members/logout").permitAll() // 로그인, 로그아웃은 인증 없이 허용
+                .requestMatchers(HttpMethod.POST, "/api/members/join").permitAll() // 회원 가입은 인증 없이 허용
+                .requestMatchers("/api/adm/**").hasRole("ADMIN") // 관리자 API는 ADMIN 권한이 있는 사용자만 접근 허용
+                .requestMatchers("/api/**").authenticated() // 나머지 API는 인증된 사용자만 접근 허용
                 .anyRequest().permitAll()
             )
                 .csrf(csrf -> csrf.disable());
