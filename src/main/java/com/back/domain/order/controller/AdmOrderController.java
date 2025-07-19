acfa8a0b4e7033a2868989ac2a2e95909110671f
rladbstn0 <140579355+rladbstn0@users.noreply.github.com>
2025-07-19 14:03:37 +0900

    [feat] 주문 단건 조회(관리자 권한)#1 (#109)
    * [feat] 주문 단건 조회(관리자 권한)
    
    * [refactor] 테스트 코드 수정

--- src/main/java/com/back/domain/order/controller/AdmOrderController.java
+++ src/main/java/com/back/domain/order/controller/AdmOrderController.java
@@ -1,6 +1,7 @@
 package com.back.domain.order.controller;
 
 import com.back.domain.order.dto.OrderDto;
+import com.back.domain.order.dto.OrderDtoWithSpecific;
 import com.back.domain.order.entity.Order;
 import com.back.domain.order.service.OrderService;
 import com.back.global.rsData.RsData;
@@ -8,7 +9,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/adm/orders")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 @Tag(name = "AdmOrderController", description = "관리자용 주문 API 컨트롤러")
 @SecurityRequirement(name = "bearerAuth")
 public class AdmOrderController {
@@ -35,4 +39,16 @@ public class AdmOrderController {
                 dtos
         );
     }
+
+    // 주문 상세 조회
+    @GetMapping("/{orderId}/detail")
+    @Operation(summary = "주문 상세 조회")
+    public RsData<OrderDtoWithSpecific> getOrderDetail(@PathVariable Long orderId) {
+        Order order = orderService.getOrderEntity(orderId);
+        return new RsData<>(
+                200,
+                "주문 상세 조회에 성공했습니다.",
+                new OrderDtoWithSpecific(order));
+    }
+
 }
